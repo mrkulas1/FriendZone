01bf3a5d0d1c4968b48f6efa57ce677e912fce0c
Ryan Klemm <rcklemm@mtu.edu>
2022-02-28 22:54:23 -0500

    Probably functional Flutter backend functions done

--- friendzone_flutter/lib/db_comm/make_post.dart
+++ friendzone_flutter/lib/db_comm/make_post.dart
@@ -10,7 +10,7 @@ import 'package:http/http.dart' as http;
 /// friendzone_post.php!!
 enum PHPFunction {
   auth,
-  getUser,
+  getCurrentUser,
   createUser,
 
   getAllEvents,
@@ -20,48 +20,43 @@ enum PHPFunction {
   joinEvent
 }
 
+// Path for all POST requests
 const String postPath =
     "https://classdb.it.mtu.edu/cs3141/team3-frien/friendzone_post.php";
+
+// List of status codes that can be returned by the PHP
+const int phpSuccessCode = 200; // Expected result will be available
+const int phpNotFoundCode = 404; // Requested data not in database
+const int phpAlreadyThereCode = 409; // Data to add already exists in database
+const int phpInternalErrorCode = 500;
+
+/// Create an error messages map and optionally specify specific error messages
+/// [notFoundMessage] is printed on response 404
+/// [alreadyThereMessage] is printed on response 409
+/// [internalErrorMessage] is printed on response 500
+Map<int, String> buildErrorMessages(
+    {String notFoundMessage = "Requested data not in database",
+    String alreadyThereMessage = "Data to add already exists in database",
+    String internalErrorMessage = "PHP Error occurred"}) {
+  return <int, String>{
+    phpNotFoundCode: notFoundMessage,
+    phpAlreadyThereCode: alreadyThereMessage,
+    phpInternalErrorCode: internalErrorMessage
+  };
+}
 
 /// Make a POST request to a given [functionID]. T is the type that is
 /// decoded from the JSON response. U is the JsonBuilder derived class
 /// that can build type T from JSON.
 ///
 /// [inputData] is a Map of parameters that will be passed to the POST
-/// [errMessage] is the String to print on POST request failure (thrown as exception)
+/// [errMessages] is the Map of messages to print on POST request failure,
+/// depending on the response code (thrown as exception)
 /// [builder] is an instance of type U used to build the returned object
-Future<T> makePostRequest<T, U extends JsonBuilder<T>>(PHPFunction functionID,
-    Map<String, dynamic> inputData, String errMessage, U builder) async {
-  // Add the functionID to the inputData
-  inputData.putIfAbsent("functionID", () => functionID.index);
-
-  final response = await http.post(
-    Uri.parse(postPath),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(inputData),
-  );
-
-  if (response.statusCode == 200) {
-    return builder.fromJson(jsonDecode(response.body));
-  } else {
-    // Handle other status codes?
-    throw Exception(errMessage);
-  }
-}
-
-/// Make a POST request to a given [functionID] that returns a list of objects.
-/// T is the type that is decoded from the JSON response. U is the
-/// JsonListBuilder derived class that can build type List<T> from JSON.
-///
-/// [inputData] is a Map of parameters that will be passed to the POST
-/// [errMessage] is the String to print on POST request failure
-/// [builder] is an instance of type U used to build the returned list
-Future<List<T>> makeListPostRequest<T, U extends JsonListBuilder<T>>(
+Future<T> makePostRequest<T, U extends JsonBuilder<T>>(
     PHPFunction functionID,
     Map<String, dynamic> inputData,
-    String errMessage,
+    Map<int, String> errMessages,
     U builder) async {
   // Add the functionID to the inputData
   inputData.putIfAbsent("functionID", () => functionID.index);
@@ -74,10 +69,61 @@ Future<List<T>> makeListPostRequest<T, U extends JsonListBuilder<T>>(
     body: jsonEncode(inputData),
   );
 
-  if (response.statusCode == 200) {
-    return builder.listFromJson(jsonDecode(response.body));
-  } else {
-    // Handle other status codes?
-    throw Exception(errMessage);
+  switch (response.statusCode) {
+    case phpSuccessCode:
+      return builder.fromJson(jsonDecode(response.body));
+    case phpNotFoundCode:
+      throw Exception(
+          errMessages[phpNotFoundCode] ?? "Requested data not in database");
+    case phpAlreadyThereCode:
+      throw Exception(errMessages[phpAlreadyThereCode] ??
+          "Data to add already exists in database");
+    case phpInternalErrorCode:
+      throw Exception(
+          errMessages[phpInternalErrorCode] ?? "PHP Error occurred");
+    default:
+      throw Exception("Unexpected error occurred");
+  }
+}
+
+/// Make a POST request to a given [functionID] that returns a list of objects.
+/// T is the type that is decoded from the JSON response. U is the
+/// JsonListBuilder derived class that can build type List<T> from JSON.
+///
+/// [inputData] is a Map of parameters that will be passed to the POST
+/// [errMessages] is the Map of messages to print on POST request failure,
+/// depending on the response code (thrown as exception)
+/// [builder] is an instance of type U used to build the returned list
+Future<List<T>> makeListPostRequest<T, U extends JsonListBuilder<T>>(
+    PHPFunction functionID,
+    Map<String, dynamic> inputData,
+    Map<int, String> errMessages,
+    U builder) async {
+  // Add the functionID to the inputData
+  inputData.putIfAbsent("functionID", () => functionID.index);
+
+  final response = await http.post(
+    Uri.parse(postPath),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(inputData),
+  );
+
+  switch (response.statusCode) {
+    case phpSuccessCode:
+      return builder.listFromJson(jsonDecode(response.body));
+    case phpNotFoundCode:
+      throw Exception(
+          errMessages[phpNotFoundCode] ?? "Requested data not in database");
+    case phpAlreadyThereCode:
+      throw Exception(errMessages[phpAlreadyThereCode] ??
+          "Data to add already exists in database");
+    case phpInternalErrorCode:
+      throw Exception(
+          errMessages[phpInternalErrorCode] ?? "PHP Error occurred");
+    default:
+      throw Exception("""Unexpected error occurred. 
+          Check your internet connection and try again.""");
   }
 }
--- friendzone_flutter/lib/db_comm/post_functions.dart
+++ friendzone_flutter/lib/db_comm/post_functions.dart
@@ -14,36 +14,107 @@ import 'dart:async';
 
 /// Authenticate the attempted login with the credentials of [email] and
 /// [password]
-Future<AuthResult> authenticate(String email, String password) {
-  throw UnimplementedError();
+Future<AuthResult> authenticate(String email, String password) async {
+  Map<String, dynamic> input = {"email": email, "password": password};
+  Map<int, String> errorMessages = buildErrorMessages();
+
+  AuthResult authResult = await makePostRequest(
+      PHPFunction.auth, input, errorMessages, AuthResultBuilder());
+
+  if (!authResult.success()) {
+    return authResult;
+  }
+
+  input.clear();
+  input = {"email": email};
+
+  CurrentUser user = await makePostRequest(
+      PHPFunction.getCurrentUser, input, errorMessages, CurrentUserBuilder());
+
+  authResult.setUser(user);
+
+  return authResult;
 }
 
 /// Register a user with the given [email], [password], [name], [intro], and
 /// [contactInfo]. Throw an exception on failure.
-Future<CurrentUser> register(
-    String email, String password, String name, String intro) {
-  throw UnimplementedError();
+Future<CurrentUser> register(String email, String password, String name,
+    String intro, String contactInfo) async {
+  Map<String, dynamic> input = {
+    "email": email,
+    "password": password,
+    "name": name,
+    "intro": intro,
+    "contact": contactInfo
+  };
+
+  Map<int, String> errorMessages = buildErrorMessages(
+      alreadyThereMessage:
+          "A user with this email is already registered. Try logging in",
+      internalErrorMessage: """Failed to create the user. 
+          Check your internet connection and try again""");
+
+  CurrentUser user = await makePostRequest(
+      PHPFunction.createUser, input, errorMessages, CurrentUserBuilder());
+
+  return user;
 }
 
 /// Get a list of the basic info for all Events. Throw an exception on failure.
-Future<List<Event>> getAllEvents() {
-  throw UnimplementedError();
+Future<List<Event>> getAllEvents() async {
+  Map<String, dynamic> input = {};
+  Map<int, String> errorMessages =
+      buildErrorMessages(internalErrorMessage: """Failed to retrieve events. 
+          Check your internet connection and try again.""");
+
+  List<Event> events = await makeListPostRequest(
+      PHPFunction.getAllEvents, input, errorMessages, EventBuilder());
+
+  return events;
 }
 
 /// Get all information about one event. Throw an exception on failure.
-Future<Event> getDetailedEvent(int eventID) {
-  throw UnimplementedError();
+Future<Event> getDetailedEvent(int eventID) async {
+  Map<String, dynamic> input = {"id": eventID};
+
+  Map<int, String> errorMessages = buildErrorMessages(
+      notFoundMessage: "Requested event was not found.",
+      internalErrorMessage: """Failed to retrieve event data. 
+    Check your internet connection and try again.""");
+
+  Event event = await makePostRequest(
+      PHPFunction.getDetailedEvent, input, errorMessages, EventBuilder());
+
+  return event;
 }
 
 /// Create a new event with the given owner [userEmail], [title], [description],
-/// number of [slots], and [category].
+/// [location], [time], number of [slots], and [category].
 Future<Event> createEvent(String userEmail, String title, String description,
-    int slots, int category) {
-  throw UnimplementedError();
+    String location, String time, int slots, int category) async {
+  Map<String, dynamic> input = {
+    "userEmail": userEmail,
+    "title": title,
+    "description": description,
+    "location": location,
+    "time": time,
+    "slots": slots,
+    "category": category
+  };
+
+  Map<int, String> errorMessages =
+      buildErrorMessages(internalErrorMessage: """Failed to create the event. 
+      Check your internetconnection and try again.""");
+
+  Event event = await makePostRequest(
+      PHPFunction.createEvent, input, errorMessages, EventBuilder());
+
+  return event;
 }
 
 /// Add the user with email [userEmail] to the event with ID [eventID], with
-/// a given [comment].
-Future<bool> joinEvent(String userEmail, int eventID, String comment) {
+/// a given [comment]. Will update the existing join comment if the user has
+/// already joined the event.
+Future<bool> joinEvent(String userEmail, int eventID, String comment) async {
   throw UnimplementedError();
 }
