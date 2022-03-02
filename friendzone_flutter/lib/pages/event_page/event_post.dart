3a5f406bc521fcd7c189a235bb4ad96ba7787207
Ryan Klemm <rcklemm@mtu.edu>
2022-03-02 13:00:39 -0500

    Fix rename

--- friendzone_flutter/lib/pages/event_page/event_post.dart
+++ friendzone_flutter/lib/pages/event_page/event_post.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:friendzone_flutter/db_comm/post_functions.dart';
+import 'package:friendzone_flutter/db_comm/post_request_functions.dart';
 
-class EventPostPage extends StatelessWidget{
-  void click(){}
+class EventPostPage extends StatelessWidget {
+  void click() {}
 
   @override
   Widget build(BuildContext context) {
@@ -23,19 +23,27 @@ class EventPostPage extends StatelessWidget{
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const SizedBox(height: 30,),
-                    const Text("Friend Zone",
-                      style: TextStyle(
-                          fontSize: 28,
-                          fontWeight:FontWeight.bold
-                      ),),
-                    const SizedBox(height: 20,),
-                    const Text("Create an Event",
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Text(
+                      "Friend Zone",
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      "Create an Event",
                       style: TextStyle(
                         color: Color.fromARGB(66, 5, 5, 5),
                         fontSize: 20,
-                      ),),
-                    const SizedBox(height: 30,),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
                     Container(
                       width: 260,
                       height: 60,
@@ -43,9 +51,9 @@ class EventPostPage extends StatelessWidget{
                         decoration: InputDecoration(
                             labelText: "Event Name",
                             border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(3)),
-                            )
-                        ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(3)),
+                            )),
                       ),
                     ),
                     Container(
@@ -55,9 +63,9 @@ class EventPostPage extends StatelessWidget{
                         decoration: InputDecoration(
                             labelText: "Location",
                             border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(3)),
-                            )
-                        ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(3)),
+                            )),
                       ),
                     ),
                     Container(
@@ -67,9 +75,9 @@ class EventPostPage extends StatelessWidget{
                         decoration: InputDecoration(
                             labelText: "Number of Slots",
                             border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(3)),
-                            )
-                        ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(3)),
+                            )),
                       ),
                     ),
                     Container(
@@ -79,9 +87,9 @@ class EventPostPage extends StatelessWidget{
                         decoration: InputDecoration(
                             labelText: "Date/Time",
                             border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(3)),
-                            )
-                        ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(3)),
+                            )),
                       ),
                     ),
                     Container(
@@ -91,9 +99,9 @@ class EventPostPage extends StatelessWidget{
                         decoration: InputDecoration(
                             labelText: "Category",
                             border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(3)),
-                            )
-                        ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(3)),
+                            )),
                       ),
                     ),
                     Container(
@@ -103,38 +111,39 @@ class EventPostPage extends StatelessWidget{
                         decoration: InputDecoration(
                             labelText: "Description of Event",
                             border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(3)),
-                            )
-                        ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(3)),
+                            )),
                       ),
                     ),
                     ElevatedButton(
-                    
-                            onPressed: () => {
-                              // createEvent(userEmail, title, description, location, time, slots, category)
-                              // Use for Create Event
-                            },
-                            style: ButtonStyle(
-                            backgroundColor:MaterialStateProperty.all<Color>(
-                              Color.fromARGB(255, 255, 204, 0)
-                            ),
-                        
-                        ),
-                            child: Container(
-                          width: 220,
-                      height: 60,
+                      onPressed: () => {
+                        // createEvent(userEmail, title, description, location, time, slots, category)
+                        // Use for Create Event
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Color.fromARGB(255, 255, 204, 0)),
+                      ),
+                      child: Container(
+                        width: 220,
+                        height: 60,
                         alignment: Alignment.center,
                         child: const Padding(
                           padding: EdgeInsets.all(12.0),
-                          child: Text('Create Event',
-                            style: TextStyle(color: Colors.white,
+                          child: Text(
+                            'Create Event',
+                            style: TextStyle(
+                                color: Colors.white,
                                 fontSize: 20,
                                 fontWeight: FontWeight.bold),
                           ),
                         ),
                       ),
-                          ),
-                    const SizedBox(height: 12,),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
                   ],
                 ),
               )
