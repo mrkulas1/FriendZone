1ecacb988d95faa67e9cb92bda4a707def81a579
Ben Drees <50091002+ben1894@users.noreply.github.com>
2022-03-02 17:45:29 -0500

    Login Page Field Validation

--- friendzone_flutter/lib/pages/login_page/login.dart
+++ friendzone_flutter/lib/pages/login_page/login.dart
@@ -18,6 +18,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _loginFormKey = GlobalKey<FormState>();
 
   Future<AuthResult>? _futureAuth;
 
@@ -48,185 +49,207 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('images/Husky.jpg'), fit: BoxFit.cover)),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(
-                height: 60,
-              ),
-              Container(
-                width: 325,
-                //height: 500,
-                decoration: const BoxDecoration(
-                  color: Colors.white70,
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
+    return Form(
+    key: _loginFormKey,
+  
+    child:
+      Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('images/Husky.jpg'), fit: BoxFit.cover)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(
+                  height: 60,
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    const Text(
-                      "Friend Zone",
-                      style:
-                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const Text(
-                      "Please Login to Your Account",
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 15,
+                Container(
+                  width: 325,
+                  //height: 500,
+                  decoration: const BoxDecoration(
+                    color: Colors.white70,
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 30,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Container(
-                      width: 260,
-                      height: 60,
-                      child: TextField(
-                        controller: _emailController,
-                        decoration: const InputDecoration(
-                            suffix: Icon(
-                              FontAwesomeIcons.envelope,
-                              color: Colors.black,
-                            ),
-                            labelText: "Email Address",
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8)),
-                            )),
+                      const Text(
+                        "Friend Zone",
+                        style:
+                            TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    Container(
-                      width: 260,
-                      height: 60,
-                      child: TextField(
-                        controller: _passwordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                            suffix: Icon(
-                              FontAwesomeIcons.eyeSlash,
-                              color: Colors.black,
-                            ),
-                            labelText: "Password",
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(8)),
-                            )),
+                      const SizedBox(
+                        height: 10,
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 30, 0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          TextButton(
-                            onPressed: () => {
-                              // TODO: Nothing for now, maybe comment out
-                            },
-                            child: const Text(
-                              "Forget Password",
-                              style: TextStyle(color: Colors.black),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                        child: (_futureAuth == null)
-                            ? Container()
-                            : createLoginBuilder()),
-                    GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _futureAuth = authenticate(
-                              _emailController.text, _passwordController.text);
-                        });
-                      },
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 250,
-                        decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(50)),
-                          color: Color(0xFFFFCC00),
+                      const Text(
+                        "Please Login to Your Account",
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
                         ),
-                        child: const Padding(
-                          padding: EdgeInsets.all(12.0),
-                          child: Text(
-                            'Login',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        width: 260,
+                        height: 60,
+                        child: TextFormField(
+                          validator: (value)
+                          {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter an email';
+                            }
+                            return null;
+                          },
+                          controller: _emailController,
+                          decoration: const InputDecoration(
+                              suffix: Icon(
+                                FontAwesomeIcons.envelope,
+                                color: Colors.black,
+                              ),
+                              labelText: "Email Address",
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(8)),
+                              )),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Container(
+                        width: 260,
+                        height: 60,
+                        child: TextFormField(
+                          validator: (value)
+                          {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a password';
+                            }
+                            return null;
+                          },
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                              suffix: Icon(
+                                FontAwesomeIcons.eyeSlash,
+                                color: Colors.black,
+                              ),
+                              labelText: "Password",
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(8)),
+                              )),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 0, 30, 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () => {
+                                // TODO: Nothing for now, maybe comment out
+                              },
+                              child: const Text(
+                                "Forget Password",
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                          child: (_futureAuth == null)
+                              ? Container()
+                              : createLoginBuilder()),
+                      GestureDetector(
+                        onTap: () {
+                          if (_loginFormKey.currentState!.validate())
+                          {
+                            setState(() {
+                              _futureAuth = authenticate(
+                                  _emailController.text, _passwordController.text);
+                            });
+                          }
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: 250,
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(50)),
+                            color: Color(0xFFFFCC00),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(12.0),
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 17,
-                    ),
-                    const Text(
-                      "Or Login using",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        IconButton(
-                            onPressed: () => {
-                                  // TODO: other auth? maybe comment out for now
-                                },
-                            icon: const Icon(
-                              FontAwesomeIcons.google,
-                              color: Colors.black,
-                            )),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
+                      const SizedBox(
+                        height: 17,
+                      ),
+                      const Text(
+                        "Or Login using",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          TextButton(
-                            onPressed: () => {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => SignUpPage()))
-                            },
-                            child: const Text(
-                              "No Account? Sign Up",
-                              style: TextStyle(color: Colors.black),
-                            ),
-                          )
+                          IconButton(
+                              onPressed: () => {
+                                    // TODO: other auth? maybe comment out for now
+                                  },
+                              icon: const Icon(
+                                FontAwesomeIcons.google,
+                                color: Colors.black,
+                              )),
                         ],
                       ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () => {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => SignUpPage()))
+                              },
+                              child: const Text(
+                                "No Account? Sign Up",
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
