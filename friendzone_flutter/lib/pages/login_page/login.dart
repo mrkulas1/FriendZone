ccec1a3a2e1f5c269cea39c318d4a730e1ee62ec
Ryan Klemm <rcklemm@mtu.edu>
2022-03-16 13:45:21 -0400

    Login bypass for debugging

--- friendzone_flutter/lib/pages/login_page/login.dart
+++ friendzone_flutter/lib/pages/login_page/login.dart
@@ -256,6 +256,20 @@ class _LoginPageState extends State<LoginPage> {
             ),
           ),
         ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.web_rounded),
+          backgroundColor: globals.friendzoneYellow,
+          onPressed: () {
+            authenticate("test@mtu.edu", "Password123").then((value) {
+              globals.activeUser = value.getUser();
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                          const EventViewAllPage()));
+            });
+          },
+        ),
       ),
     );
   }
