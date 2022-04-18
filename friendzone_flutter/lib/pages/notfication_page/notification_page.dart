d68ef6e9d2b158f57a52159f72cf6e6c769385c6
Thawng Hmung <tthmung@mtu.edu>
2022-04-17 22:43:21 -0400

    Notification complete

--- friendzone_flutter/lib/pages/notfication_page/notification_page.dart
+++ friendzone_flutter/lib/pages/notfication_page/notification_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:friendzone_flutter/db_comm/post_request_functions.dart';
 import 'package:friendzone_flutter/global_header.dart';
+import 'package:friendzone_flutter/models/notification.dart' as noti;
 
 class ViewNotfication extends StatefulWidget {
   const ViewNotfication({Key? key}) : super(key: key);
@@ -9,13 +12,79 @@ class ViewNotfication extends StatefulWidget {
 }
 
 class _ViewNotfication extends State<ViewNotfication> {
+  Future<List<noti.Notification>>? _notfication;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _notfication = getNotifications();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const Header(),
       backgroundColor: const Color(0xFFDCDCDC), // Background color
       body: Container(
-        child: Text("Hello World"),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(FontAwesomeIcons.bell),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("Notifications"),
+                  ],
+                ),
+                Container(
+                  child: _notfication == null
+                      ? Container()
+                      : FutureBuilder<List<noti.Notification>>(
+                          future: _notfication,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              return Expanded(
+                                child: ListView.builder(
+                                  itemCount: snapshot.data!.length,
+                                  itemBuilder: (context, index) {
+                                    IconData _envelope =
+                                        FontAwesomeIcons.envelopeCircleCheck;
+                                    if (snapshot.data![index].seen) {
+                                      _envelope =
+                                          FontAwesomeIcons.envelopeCircleCheck;
+                                    }
+                                    return ListTile(
+                                      leading: Icon(_envelope),
+                                      title: Text(
+                                          snapshot.data![index].eventTitle),
+                                      subtitle:
+                                          Text("${snapshot.data![index].text}\n"
+                                              "${snapshot.data![index].time}"),
+                                    );
+                                  },
+                                ),
+                              );
+                            } else if (snapshot.hasError) {
+                              return Text("${snapshot.error!}");
+                            }
+                            return const CircularProgressIndicator();
+                          }),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
